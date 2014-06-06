d526c3e4eb41e069e9e5fe33a765392b1b25a810
Alberto G. Corona <agocorona@gmail.com>
2014-06-06 09:41:09 +0200

    Main.hs example

---
+++ Main.hs
@@ -0,0 +1,19 @@
+module Main where
+import Haste.DOM
+import Builder
+import Control.Monad.State
+import Data.Monoid
+import Prelude hiding (div)
+
+main= do
+  withElem "idelem" . build $ do
+    div $ do
+         div  $ do
+               p "hello"
+               nelem "p" `attr` ("style","color:red")  `child`  "world"
+
+  return ()
+
+div cont=  nelem "div" `child`  cont
+
+p cont = nelem "p"  `child`  cont
