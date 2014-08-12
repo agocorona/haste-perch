78ac5c2d03f226a6e07a8121e386c05e5f32bd12
Alberto G. Corona <agocorona@gmail.com>
2014-08-12 19:29:03 +0200

    main example

--- Main.hs
+++ Main.hs
@@ -1,14 +1,12 @@
 module Main where
 import Haste.DOM
 import Haste.Perch
-import Haste.Perch.Literate
 import Data.Monoid
 import Prelude hiding (div)
 
 
 
 main= do
-  lit "hello <b>world<b>"
   body <- getBody
   build' body $ do
     div $ do
